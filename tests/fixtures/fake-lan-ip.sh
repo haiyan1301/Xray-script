8dc7db097f1ff20a01d178fcd8adb66d46c7cd37
unknown <microsoft365dev@gmail.com>
2026-07-31 20:07:50 +0800

    chore: exclude test scripts from repository

--- tests/fixtures/fake-lan-ip.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-printf '%s\n' "$*" >>"${IP_LOG}"
-if [[ -n "${IP_FAIL_MATCH:-}" && "$*" == *"${IP_FAIL_MATCH}"* ]]; then
-    exit 23
-fi
-if [[ "$*" == 'link show dev xray0' ]]; then
-    exit 0
-fi
-exit 0
